c6c21b7520d9b6ef1d548e647ba02cf83c4df39b
visa111 <visaruttor@gmail.com>
2018-07-31 10:12:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!-- This file allows you to host this page as a static file on Heroku -->
 <?php
-include "connect_liff.php";
+//include "connect_liff.php";
 ?>
 <?php header( 'Location: /index.html' ) ;  ?>
