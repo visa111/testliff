6449bc0e8243303a972dc2f4342184cecb92d1c7
visa111 <visaruttor@gmail.com>
2018-07-31 11:41:00 +0700

    Add files via upload

---
+++ client/footer.php
@@ -0,0 +1,7 @@
+    <div id="footer" class="text-center">
+    <br>
+        Copyright &copy; 2018 <?php //echo date('Y'); ?>&nbsp;<?php echo (string) 'www.itsm.com'; ?>&nbsp;All Rights Reserved.
+    </div>
+
+    </body>
+</html>
