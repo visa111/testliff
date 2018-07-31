5ccd2f439ac2878b26300b26acdc0e2f4409b79c
visa111 <visaruttor@gmail.com>
2018-07-31 14:28:31 +0700

    Update testlocation.php

--- client/testlocation.php
+++ client/testlocation.php
@@ -1,38 +1,28 @@
+<!DOCTYPE html>
 <html>
-<head>
-<title>ThaiCreate.Com Tutorials</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<script src="http://code.jquery.com/jquery-latest.min.js"></script>
-</head>
 <body>
 
-<input type="button" name="btnGetLocation" id="btnGetLocation" value="Get Location">
-<br/> <br />
-<div id="location"></div>
+<p>Click the button to get your coordinates.</p>
 
+<button onclick="getLocation()">Try It</button>
 
-<script type="text/javascript">
+<p id="demo"></p>
 
-	function getLocation() {
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(showPosition);
-		} else { 
-			$('#location').html("Geolocation is not supported by this browser.");
-		}
-	}
+<script>
+var x = document.getElementById("demo");
 
-	function showPosition(position) {
-		var location = "Latitude: " + position.coords.latitude +  "<br>Longitude: " + position.coords.longitude;
-		$('#location').html(location);
-	}
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
 
-	$(document).ready(function() {
-		
-		$("#btnGetLocation").click(function() {	
-				getLocation();
-		});
-
-	});
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+}
 </script>
 
 </body>
